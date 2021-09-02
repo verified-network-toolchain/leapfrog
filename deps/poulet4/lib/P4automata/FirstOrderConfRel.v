bc3393b010cbe469ecc13cf6efe37064534cf3a3
John Sarracino <jsarracino@cornell.edu>
2021-09-01 21:15:59 -0400

    add bool + BV firstorder model

--- deps/poulet4/lib/P4automata/FirstOrderConfRel.v
+++ deps/poulet4/lib/P4automata/FirstOrderConfRel.v
@@ -5,7 +5,7 @@ Require Import Poulet4.P4automata.PreBisimulationSyntax.
 Require Import Poulet4.P4automata.P4automaton.
 Require Import Poulet4.P4automata.FirstOrder.
 
-Print conf_rel.
+Require Import Coq.Lists.List.
 
 Section AutModel.
   Set Implicit Arguments.
@@ -100,7 +100,9 @@ Section AutModel.
   Definition l2t {A} (l: list A) : n_tuple A (List.length l).
   Admitted.
 
-  Definition be_sort {c} (b1 b2 h) (e: bit_expr H c) :=
+
+
+  (* Definition be_sort {c} (b1 b2 h) (e: bit_expr H c) :=
     match e with
     | BELit _ _ l => Bits (List.length l)
     | BEBuf a => Bits b1
@@ -117,6 +119,209 @@ Section AutModel.
     | BEVar b => _
     | BESlice e hi lo => _
     | BEConcat e1 e2 => _
-    end.
+    end. *)
 
 End AutModel.
+
+Section BitsBV.
+
+  Require Import Coq.Init.Specif.
+  Require Import Coq.micromega.Lia.
+
+  Inductive bv_sorts: Type :=
+  | BVBits (len: {n: nat | n > 0})
+  | BVBool.
+
+  Definition pos := {n: nat | n > 0}.
+
+  Program Definition plus (l: {n: nat | n > 0}) (r: {n: nat | n > 0}) : {n: nat | n > 0} := l + r.
+  Next Obligation.
+  lia.
+  Defined.
+
+  (* Definition gt (l: {n: nat | n > 0}) (r: {n: nat | n > 0})  := . *)
+  
+  Program Definition sub_add_one (i : nat) (j: nat) : (i >= j) -> {n: nat | n > 0} := fun pf => i - j + 1.
+  Next Obligation.
+  lia.
+  Defined.
+
+  Inductive bool_bop: Type := 
+  | BImpl | BXor.
+
+  Inductive bv_funs: arity bv_sorts -> bv_sorts -> Type :=
+  | BoolLit: forall (b: bool), bv_funs [] BVBool
+  | BVLit: forall (bv: list bool) (pf: length bv > 0), bv_funs [] (BVBits (exist _ (length bv) pf))
+  | BoolBop: forall (bop: bool_bop), bv_funs [BVBool; BVBool] BVBool
+  | BVConcat : forall i j, bv_funs [BVBits i; BVBits j] (BVBits (plus i j))
+  | BVExtract : forall (i j: nat) (m n : {x: nat | x > 0}) (pf: i >= j),
+    proj1_sig m > i -> 
+    n = sub_add_one pf  ->
+    bv_funs [BVBits m] (BVBits n).
+
+  Inductive bv_rels: arity bv_sorts -> Type := .
+
+  Definition bv_sig : signature := 
+    {| sig_sorts := bv_sorts; sig_funs := bv_funs; sig_rels := bv_rels |}.
+
+
+    (* Record model :=
+    { mod_sorts: sig.(sig_sorts) -> Type;
+      mod_fns: forall args ret,
+          sig.(sig_funs) args ret ->
+          HList.t mod_sorts args ->
+          mod_sorts ret;
+      mod_rels: forall args,
+          sig.(sig_rels) args ->
+          HList.t mod_sorts args ->
+          Prop }. *)
+
+  
+  Definition bitvector (n: nat) : Type := {bs: list bool | length bs = n}.
+  
+
+  Definition bv_mod_sorts (sig: bv_sig.(sig_sorts)) : Type := 
+    match sig with 
+    | BVBool => bool
+    | BVBits n => bitvector (proj1_sig n)
+    end.
+
+  (* inclusive slicing *)
+
+  Definition slice {A} (hi lo: nat) (xs: list A) := firstn (hi - lo + 1) (skipn (lo - 1) xs).
+
+  (* Compute (slice 3 2 (1::2::3::4::5::nil)). *)
+
+  Lemma slice_len:
+    forall A hi lo (xs : list A),
+      length xs > hi -> 
+      hi >= lo -> 
+      length (slice hi lo xs) = hi - lo + 1.
+  Proof.
+    intros.
+    induction hi; induction lo; simpl.
+    - unfold slice.
+      simpl.
+      destruct xs; [exfalso; simpl in *; lia | simpl; trivial].
+    - exfalso.
+      inversion H0.
+    - unfold slice.
+      simpl (0 - 1).
+      erewrite skipn_O.
+      simpl (S hi - 0 + 1).
+      erewrite firstn_length.
+
+      eapply min_l.
+      lia.
+
+    - admit.
+  Admitted.
+
+  Definition bv_mod_fns {args: arity (sig_sorts bv_sig)} {ret: sig_sorts bv_sig} 
+    (sf: bv_sig.(sig_funs) args ret) 
+    (env: HList.t bv_mod_sorts args) : bv_mod_sorts ret.
+  destruct sf eqn:?.
+  - exact b.
+  - simpl. 
+    eapply (exist _ bv).
+    trivial.
+  - inversion env.
+    simpl in X.
+    clear env H0 a H1 rest.
+    inversion X0.
+    simpl in X1.
+    clear H0 H1 X0 rest X2 a.
+    destruct bop eqn:?.
+    * exact (implb X X1).
+    * exact (xorb X X1).
+  - inversion env.
+    simpl in X.
+    clear env H0 a H1 rest.
+    inversion X0.
+    simpl in X1.
+    clear H0 H1 X0 rest X2 a.
+
+    destruct X.
+    destruct X1.
+
+    eapply (exist _ (x ++ x0)).
+
+    simpl.
+  
+    rewrite <- e.
+    rewrite <- e0.
+
+    eapply app_length.
+  - rewrite e.
+    simpl.
+
+    inversion env.
+    simpl in X.
+    clear env H0 a H1 rest X0.
+
+    destruct X.
+
+    eapply (exist _ (slice i j x)).
+
+    eapply slice_len; lia.
+  Defined.
+
+  Definition bv_mod_rels {args}
+    (sig_args: bv_sig.(sig_rels) args) 
+    (env: HList.t bv_mod_sorts args) : Prop := False.
+
+  Definition bv_model : model bv_sig.
+  refine {| FirstOrder.mod_sorts := bv_mod_sorts; 
+       FirstOrder.mod_fns := _; FirstOrder.mod_rels := _ |}.
+
+  - intros. 
+    exact (bv_mod_fns H X).
+  - intros.
+    exact (bv_mod_rels X X0).
+  Defined.
+
+  (* true = true *)
+
+  Definition ex0 : fm bv_sig (CEmp _).
+  refine (
+    FEq _ (CEmp _) _ _ _
+  ).
+  - refine (TFun _ _ _ _ _ _).
+    * refine (BoolLit true).
+    * constructor.
+  - refine (TFun _ _ _ _ _ _).
+    * refine (BoolLit true).
+    * constructor.
+  Defined.
+
+  Lemma ex0_interp : interp_fm bv_sig bv_model (CEmp _) (VEmp _ _) ex0.
+  Proof.
+  Admitted.
+  
+
+  (* forall x, x -> x *)
+
+  (* Definition ex1 := 
+    FForall _ _ _ TEq.
+
+  Check ex1. *)
+  
+
+
+  (* forall a b c, (a -> b) -> (b -> c) -> (a -> c) *)
+
+
+  
+  (* | KeyLit: H -> funs [] Key
+  | Concat: forall n m, funs [Bits n; Bits m] (Bits (n + m))
+  | Slice: forall n hi lo,
+      funs [Bits n] (Bits (1 + hi - lo))
+  | Update: forall n, funs [Store; Key; Bits n] Store
+  | State1: funs [ConfigPair] State
+  | Store1: funs [ConfigPair] Store
+  | State2: funs [ConfigPair] State
+  | Store2: funs [ConfigPair] Store. *)
+
+
+
+End BitsBV.
