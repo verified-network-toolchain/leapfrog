2416a5d2f59f1eb4eede5ed129c8d57232400f78
Ryan Doenges <me@ryandoeng.es>
2021-09-07 11:14:18 -0400

    fix some stuff in FirstOrderConfRel

--- deps/poulet4/lib/P4automata/FirstOrderConfRel.v
+++ deps/poulet4/lib/P4automata/FirstOrderConfRel.v
@@ -58,17 +58,27 @@ Section AutModel.
   Definition tm ctx := FirstOrder.tm sig ctx.
   Definition tms ctx := FirstOrder.tms sig ctx.
 
+  Definition conf' (n: nat) :=
+    {c: conf | c.(conf_buf_len _) = n}.
+
   Definition mod_sorts (s: sig_sorts sig) : Type :=
     match s with
     | Bits n => n_tuple bool n
     | State => states (P4A.interp S H a) + bool
     | Store => store (P4A.interp S H a)
     | Key n => H n
-    | ConfigPair n m => conf * conf
+    | ConfigPair n m => conf' n * conf' m
     end.
+
+  Definition l2t {A} (l: list A) : n_tuple A (List.length l).
+  Admitted.
+
+  Definition n_zeroes (n: nat) : n_tuple bool n.
+  Admitted.
 
   Notation "x ::: xs" := (HList.HCons _ x xs) (at level 60, right associativity).
 
+  Obligation Tactic := idtac.
   Equations mod_fns
              {params ret}
              (f: sig_funs sig params ret)
@@ -80,19 +90,38 @@ Section AutModel.
         n_tuple_concat xs ys;
       mod_fns (Slice n hi lo) (xs ::: hnil) :=
         n_tuple_slice hi lo xs;
-      mod_fns (Lookup n) (store ::: k ::: hnil) :=
-        match P4A.find H (P4A.HRVar (existT _ n k)) store with
-        | P4A.VBits _ v => v
+      mod_fns (Lookup n) (store ::: key ::: hnil) :=
+        match P4A.find H key store with
+        | Some (P4A.VBits _ v) => v
+        | None => n_zeroes n
         end;
       mod_fns (Update n) (store ::: k ::: v ::: hnil) :=
-        P4A.assign (P4A.HRVar k) (P4A.VBits (t2l _ _ v)) store;
-      mod_fns (State1 _ _) ((q1, q2) ::: hnil) := fst (fst q1);
-      mod_fns (Store1 _ _) ((q1, q2) ::: hnil) := snd (fst q1);
-      mod_fns (State2 _ _) ((q1, q2) ::: hnil) := fst (fst q2);
-      mod_fns (Store2 _ _) ((q1, q2) ::: hnil) := snd (fst q2);
-      mod_fns (Buf1 _ _) ((q1, q2) ::: hnil) := snd (snd q1);
-      mod_fns (Buf2 _ _) ((q2, q2) ::: hnil) := snd (snd q2)
+        P4A.assign _ k (P4A.VBits _ v) store;
+      mod_fns (State1 _ _) ((q1, q2) ::: hnil) := (proj1_sig q1).(conf_state _);
+      mod_fns (Store1 _ _) ((q1, q2) ::: hnil) := (proj1_sig q1).(conf_store _);
+      mod_fns (State2 _ _) ((q1, q2) ::: hnil) := (proj1_sig q2).(conf_state _);
+      mod_fns (Store2 _ _) ((q1, q2) ::: hnil) := (proj1_sig q2).(conf_store _);
+      mod_fns (Buf1 n m) ((q1, q2) ::: hnil) := _;
+      mod_fns (Buf2 n m) ((q2, q2) ::: hnil) := _
     }.
+  Next Obligation.
+    unfold conf'.
+    intros.
+    pose ((proj1_sig q1).(conf_buf _)).
+    destruct q1.
+    simpl in *.
+    rewrite e in n0.
+    exact n0.
+  Defined.
+  Next Obligation.
+    unfold conf'.
+    intros.
+    pose ((proj1_sig q2).(conf_buf _)).
+    destruct q2.
+    simpl in *.
+    rewrite e in n0.
+    exact n0.
+  Defined.
 
   Fixpoint tr_bctx (b: bctx): ctx sig :=
     match b with
@@ -100,26 +129,34 @@ Section AutModel.
     | BCSnoc b size => CSnoc _ (tr_bctx b) (Bits size)
     end.
 
-  Definition l2t {A} (l: list A) : n_tuple A (List.length l).
-  Admitted.
-
-  Definition be_sort {c} (b1 b2 h) (e: bit_expr H c) :=
+  Fixpoint be_size {c} b1 b2 (e: bit_expr H c) : nat :=
     match e with
-    | BELit _ _ l => Bits (List.length l)
-    | BEBuf a => Bits b1
-    | BEHdr a h =>
-    | BEVar b => _
-    | BESlice e hi lo => _
-    | BEConcat e1 e2 => _
-
-  Fixpoint tr_bit_expr {c} (e: bit_expr H c) : tm sig (tr_bctx c) (be_sort e) :=
-    match e with
-    | BELit _ _ l => TFun _ _ _ (BitsLit (List.length l) (l2t l))
-    | BEBuf a => _
-    | BEHdr a h => _
-    | BEVar b => _
-    | BESlice e hi lo => _
-    | BEConcat e1 e2 => _
+    | BELit _ _ l => List.length l
+    | BEBuf _ _ side =>
+      match side with
+      | Left => b1
+      | Right => b2
+      end
+    | BEHdr _ a (P4A.HRVar h) => projT1 h
+    | BEVar _ x => check_bvar x
+    | BESlice e hi lo =>
+      let n := be_size b1 b2 e in
+      Nat.min (1 + hi) n - lo
+    | BEConcat e1 e2 =>
+      be_size b1 b2 e1 + be_size b1 b2 e2
     end.
+
+  Definition be_sort {c} b1 b2 (e: bit_expr H c) : sorts :=
+    Bits (be_size b1 b2 e).
+  
+  Equations tr_bit_expr {c} b1 b2 (e: bit_expr H c) : tm (tr_bctx c) (be_sort b1 b2 e) :=
+    { tr_bit_expr b1 b2 (BELit _ _ l) :=
+        TFun sig _ _ _ (BitsLit (List.length l) (l2t l)) (TSNil _ _);
+      tr_bit_expr b1 b2 (BEBuf _ _ side) := _;
+      tr_bit_expr b1 b2 (BEHdr _ a h) := _;
+      tr_bit_expr b1 b2 (BEVar _ b) := _;
+      tr_bit_expr b1 b2 (BESlice e hi lo) := _;
+      tr_bit_expr b1 b2 (BEConcat e1 e2) := _
+    }.
 
 End AutModel.
