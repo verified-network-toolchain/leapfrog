5446d7ef225541a8977b379a4bcc0bd2c36c4931
Tobias Kappé <tobias@kap.pe>
2021-10-07 11:39:56 +0200

    Wrap appeals to typeclasses in once.

--- deps/poulet4/lib/P4automata/BisimChecker.v
+++ deps/poulet4/lib/P4automata/BisimChecker.v
@@ -339,7 +339,7 @@ Ltac skip_bisim :=
   | |- pre_bisimulation ?a ?wp ?i ?R (?C :: _) _ =>
     let H := fresh "H" in
     assert (H: interp_entailment a i ({| e_prem := R; e_concl := C |}));
-    [idtac | 
+    [idtac |
       apply PreBisimulationSkip with (H0:=left H);
       [ exact I | ];
       clear H
@@ -388,9 +388,9 @@ Ltac verify_interp top top' :=
     [
       eapply simplify_entailment_correct with (i := top');
       eapply compile_simplified_entailment_correct;
-      [ typeclasses eauto 2 | typeclasses eauto 2 | typeclasses eauto 2 |];
+      [ once typeclasses eauto | once typeclasses eauto | once typeclasses eauto |];
       eapply FirstOrderConfRelSimplified.simplify_concat_zero_fm_corr;
-      [ typeclasses eauto 2 | typeclasses eauto 2 |];
+      [ once typeclasses eauto | once typeclasses eauto |];
 
       time "reduce goal" crunch_foterm;
 
@@ -424,9 +424,9 @@ Ltac close_bisim top' :=
   apply PreBisimulationClose;
   eapply simplify_entailment_correct' with (i := top');
   eapply compile_simplified_entailment_correct';
-  [ typeclasses eauto 2 | typeclasses eauto 2 | typeclasses eauto 2 |];
+  [ once typeclasses eauto | once typeclasses eauto | once typeclasses eauto |];
   eapply FirstOrderConfRelSimplified.simplify_concat_zero_fm_corr;
-  [ typeclasses eauto 2 | typeclasses eauto 2 |];
+  [ once typeclasses eauto | once typeclasses eauto |];
   crunch_foterm;
   match goal with
   | |- ?X => time "smt check pos" check_interp_pos X; admit
