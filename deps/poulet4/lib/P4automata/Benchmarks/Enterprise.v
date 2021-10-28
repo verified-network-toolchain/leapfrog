e4ffcd8ec11f68a5ad684d0b6aafb5d79d0b5fbc
Ryan Doenges <me@ryandoeng.es>
2021-10-28 14:48:22 -0400

    fix slicing in Enterprise.v example

--- deps/poulet4/lib/P4automata/Benchmarks/Enterprise.v
+++ deps/poulet4/lib/P4automata/Benchmarks/Enterprise.v
@@ -51,42 +51,48 @@ Definition states (s: state) : P4A.state state header :=
   match s with
   | ParseEth =>
     {| st_op := extract(HdrEth);
-       st_trans := transition select (| (EHdr HdrEth)[1--0] |)
-                              {{ [| exact #b|0|0 |] ==> inl ParseVLAN0 ;;;
-                                 [| exact #b|0|1 |] ==> inl ParseIPv4 ;;;
-                                 [| exact #b|1|1 |] ==> inl ParseIPv6 ;;;
+       st_trans := transition select (| (EHdr HdrEth)[111--96] |)
+                              {{ [| hexact 0x8100 |] ==> inl ParseVLAN0 ;;;
+                                 [| hexact 0x9100 |] ==> inl ParseVLAN0 ;;;
+                                 [| hexact 0x9200 |] ==> inl ParseVLAN0 ;;;
+                                 [| hexact 0x9300 |] ==> inl ParseVLAN0 ;;;
+                                 [| hexact 0x0800 |] ==> inl ParseIPv4 ;;;
+                                 [| hexact 0x86dd |] ==> inl ParseIPv6 ;;;
                                  reject }}
     |}
   | ParseVLAN0 =>
     {| st_op := extract(HdrVLAN0) ;
-       st_trans := transition select (| (EHdr HdrVLAN0)[1--0] |)
-                              {{ [| exact #b|0|0 |] ==> inl ParseVLAN1 ;;;
-                                 [| exact #b|0|1 |] ==> inl ParseIPv4 ;;;
-                                 [| exact #b|1|1 |] ==> inl ParseIPv6 ;;;
+       st_trans := transition select (| (EHdr HdrVLAN0)[159--144] |)
+                              {{ [| hexact 0x8100 |] ==> inl ParseVLAN1 ;;;
+                                 [| hexact 0x9100 |] ==> inl ParseVLAN1 ;;;
+                                 [| hexact 0x9200 |] ==> inl ParseVLAN1 ;;;
+                                 [| hexact 0x9300 |] ==> inl ParseVLAN1 ;;;
+                                 [| hexact 0x0800 |] ==> inl ParseIPv4 ;;;
+                                 [| hexact 0x86dd |] ==> inl ParseIPv6 ;;;
                                  reject }}
     |}
   | ParseVLAN1 =>
     {| st_op := extract(HdrVLAN1) ;
-       st_trans := transition select (| (EHdr HdrVLAN1)[1--0] |)
-                              {{ [| exact #b|0|1 |] ==> inl ParseIPv4 ;;;
-                                 [| exact #b|1|1 |] ==> inl ParseIPv6 ;;;
+       st_trans := transition select (| (EHdr HdrVLAN1)[159--144] |)
+                              {{ [| hexact 0x0800 |] ==> inl ParseIPv4 ;;;
+                                 [| hexact 0x86dd |] ==> inl ParseIPv6 ;;;
                                  reject }}
     |}
   | ParseIPv4 =>
     {| st_op := extract(HdrIPv4);
-       st_trans := transition select (| (EHdr HdrIPv4)[1--0] |)
-                              {{ [| exact #b|0|0 |] ==> inl ParseUDP;;;
-                                 [| exact #b|0|1 |] ==> inl ParseTCP;;;
-                                 [| exact #b|1|0 |] ==> inl ParseICMP;;;
+       st_trans := transition select (| (EHdr HdrIPv4)[79--72] |)
+                              {{ [| hexact 1 |] ==> inl ParseICMP;;;
+                                 [| hexact 6 |] ==> inl ParseTCP;;;
+                                 [| hexact 17 |] ==> inl ParseUDP;;;
                                  accept
                               }}
     |}
   | ParseIPv6 =>
     {| st_op := extract(HdrIPv6);
-       st_trans := transition select (| (EHdr HdrIPv6)[1--0] |)
-                              {{ [| exact #b|0|0 |] ==> inl ParseUDP;;;
-                                 [| exact #b|0|1 |] ==> inl ParseTCP;;;
-                                 [| exact #b|1|0 |] ==> inl ParseICMP;;;
+       st_trans := transition select (| (EHdr HdrIPv6)[55--48] |)
+                              {{ [| hexact 1 |] ==> inl ParseICMP;;;
+                                 [| hexact 6 |] ==> inl ParseTCP;;;
+                                 [| hexact 17 |] ==> inl ParseUDP;;;
                                  accept
                               }}
     |}
