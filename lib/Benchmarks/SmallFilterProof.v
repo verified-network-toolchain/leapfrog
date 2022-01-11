d2f21adec0674d666573d1b7ff65212959aa7d30
Tobias Kappé <tobias@kap.pe>
2022-01-11 17:08:14 +0100

    Trying to automate SmallFilterProof a bit more.

--- lib/Benchmarks/SmallFilterProof.v
+++ lib/Benchmarks/SmallFilterProof.v
@@ -26,6 +26,64 @@ RegisterEnvCtors
   (BigBits.Suf, FirstOrderConfRelSimplified.Bits 1).
 *)
 
+Require Import Leapfrog.Bisimulations.Semantic.
+Require Import Leapfrog.Bisimulations.LeapsProofs.
+Require Import Leapfrog.Bisimulations.WPLeapsProofs.
+
+(* TODO: We need something like this for general sums of automata. Basically,
+   if two states in the sum automaton have the same language, then those
+   states have the same language in their original automaton. *)
+Lemma sum_thing:
+  forall (q1: IncrementalBits.state) (q2: BigBits.state),
+    lang_equiv_state
+      (a1 := P4A.interp A)
+      (a2 := P4A.interp A)
+      (inl q1)
+      (inr q2) ->
+    lang_equiv_state
+      (a1 := P4A.interp IncrementalBits.aut)
+      (a2 := P4A.interp BigBits.aut)
+      q1
+      q2.
+Proof.
+Admitted.
+
+Lemma prebisim_incremental_sep:
+  lang_equiv_state
+    (a1 := P4A.interp IncrementalBits.aut)
+    (a2 := P4A.interp BigBits.aut)
+    IncrementalBits.Start
+    BigBits.Parse
+.
+Proof.
+  apply sum_thing.
+  unfold lang_equiv_state.
+  intros.
+  match goal with
+  | |- lang_equiv ?c1t ?c2t =>
+    set (c1 := c1t);
+    set (c2 := c2t)
+  end.
+  apply bisimilar_implies_equiv.
+  apply bisimilar_iff_bisimilar_with_leaps.
+  eapply wp_leaps_implies_bisim_leaps with
+    (s1 := IncrementalBits.Start)
+    (s2 := BigBits.Parse).
+  2:{
+    (* The second goal is fairly easy to discharge; this should be doable
+       for proofs like this in general. *)
+    unfold top.
+    vm_compute r.
+    subst c1.
+    subst c2.
+    vm_compute conf_to_state_template.
+    do 6 right.
+    left.
+    reflexivity.
+  }
+  (* TODO Now how to handle the pre-bisimulation goal...? *)
+Abort.
+
 Lemma prebisim_incremental_sep:
   forall q1 q2,
     interp_conf_rel' {| cr_st := {|
@@ -43,7 +101,7 @@ Lemma prebisim_incremental_sep:
                    |} q1 q2 ->
   pre_bisimulation A
                    (wp r_states)
-                   top
+                   SmallFilterProof.top
                    []
                    (mk_init _ _ _ _ A 200 IncrementalBits.Start BigBits.Parse)
                    q1 q2.
@@ -55,6 +113,7 @@ Proof.
   vm_compute in rel0.
   subst rel0.
 
-  time "build phase" repeat (time "single step" run_bisim top top' r_states).
-  time "close phase" close_bisim top'.
-Time Admitted.
+  (* This is broken now... *)
+  time "build phase" repeat (time "single step" run_bisim SmallFilterProof.top top' r_states).
+  Fail time "close phase" close_bisim top'.
+Abort.
