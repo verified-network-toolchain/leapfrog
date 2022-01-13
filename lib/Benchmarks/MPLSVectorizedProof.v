e149bcda6bd81d92e2dcc9e3f82e784c14431d80
John Sarracino <jsarracino@cornell.edu>
2022-01-13 15:57:48 -0300

    actually add MPLS proof

--- lib/Benchmarks/MPLSVectorizedProof.v
+++ lib/Benchmarks/MPLSVectorizedProof.v
@@ -3,6 +3,8 @@ Require Import Leapfrog.Benchmarks.MPLSVectorized.
 
 Require Import Coq.Arith.PeanoNat.
 
+Require Import Leapfrog.Utils.FunctionalFP.
+
 
 Notation A := MPLSVect.aut.
 Notation conf := (P4automaton.configuration (P4A.interp A)).
@@ -11,48 +13,24 @@ Notation start_right := Unrolled.ParseMPLS.
 
 (* Eval vm_compute in (length (Reachability.valid_state_templates A)). *)
 
-Definition r_states : Reachability.state_pairs A.
-  evar (r: Reachability.state_pairs A).
-  assert (Reachability.reachable_states_wit _ start_left start_right r).
 
 
-
-Definition r_states := (Reachability.reachable_states
-                        A
-                        start_left
-                        start_right).
-
+Definition r_states : {r : Reachability.state_pairs A & Reachability.reachable_states_wit start_left start_right r}.
+  econstructor.
+  unfold Reachability.reachable_states_wit.
+  match goal with 
+  | |- fp_wit _ _ _ ?R => set (baz := R)
+  end.
+  solve_fp_wit.
+  subst baz.
+  eapply FPDone.
+  exact eq_refl.
+Defined.
                 
-
 Definition top : Relations.rel conf := fun _ _ => True.
 Definition top' : Relations.rel (state_template A) := fun _ _ => True.
 
 Declare ML Module "mirrorsolve".
-
-Lemma interp_compile_simplify: 
-  forall prem concl, 
-    interp_fm
-    (compile_valu (a := A) (VEmp _ _))
-    (compile_fm
-      (FirstOrderConfRelSimplified.simplify_eq_zero_fm
-          (FirstOrderConfRelSimplified.simplify_concat_zero_fm
-            (compile_simplified_entailment
-                (simplify_entailment
-                  {|
-                    e_prem := prem;
-                    e_concl :=
-                      concl
-                  |}))))) 
-      -> interp_entailment A top ({| e_prem := prem; e_concl := concl |}).
-Proof.
-  intros.
-  eapply simplify_entailment_correct with (i := top');
-  eapply compile_simplified_entailment_correct; simpl; intros;
-  eapply FirstOrderConfRelSimplified.simplify_concat_zero_fm_corr;
-  eapply FirstOrderConfRelSimplified.simplify_eq_zero_fm_corr;
-  eapply CompileFirstOrderConfRelSimplified.compile_simplified_fm_bv_correct.
-  auto.
-Qed.
 
 Lemma prebisim_mpls_unroll:
   forall q1 q2,
@@ -70,20 +48,29 @@ Lemma prebisim_mpls_unroll:
                       cr_rel := btrue;
                     |} q1 q2 ->
   pre_bisimulation A
-                    (wp r_states)
+                    (wp (projT1 r_states))
                     top
                     []
-                    (mk_init _ _ _ A r_depth start_left start_right)
+                    (mk_init _ _ _ _ A start_left start_right)
                     q1 q2.
 Proof.
   intros.
 
-  set (rel0 := (mk_init _ _ _ _ _ _ _)).
-  vm_compute in rel0.
-  subst rel0.
+  pose proof (Reachability.reachable_states_wit_conv (projT2 r_states)) as Hr.
+
+  unfold mk_init.
+  rewrite Hr.
+  clear Hr.
+  
+  set (foo := (List.nodup (conf_rel_eq_dec (a:=A))
+  (mk_partition Plain.state Unrolled.state
+     (Sum.Hdr Plain.header Unrolled.header)
+     (Sum.Hdr_sz Plain.sz Unrolled.sz) A _))).
+  vm_compute in foo.
+  subst foo.
 
   (* time "build phase" repeat (time "single step" run_bisim' top top' r_states interp_compile_simplify). *)
-  time "build phase" repeat (time "single step" run_bisim top top' r_states).
+  time "build phase" repeat (time "single step" run_bisim top top' (projT1 r_states)).
   time "close phase" close_bisim top'.
 Time Admitted.
 
