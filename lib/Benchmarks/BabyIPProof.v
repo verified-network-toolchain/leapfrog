75783e84cb49e2a0d8736ea6ac7ad96a0c866915
Ryan Doenges <me@ryandoeng.es>
2022-02-21 13:26:31 -0500

    do small benchmarks

--- lib/Benchmarks/BabyIPProof.v
+++ lib/Benchmarks/BabyIPProof.v
@@ -1,45 +1,13 @@
 Require Import Leapfrog.Benchmarks.ProofHeader.
 Require Import Leapfrog.Benchmarks.BabyIP.
 
-Notation H := (BabyIP1.header + BabyIP2.header).
-Notation A := BabyIP.aut.
-Notation conf := (P4automaton.configuration (P4A.interp A)).
-
-Definition r_states : {r : Reachability.state_pairs A & Reachability.reachable_states_wit BabyIP1.Start BabyIP2.Start r}.
-  econstructor.
-  unfold Reachability.reachable_states_wit.
-  solve_fp_wit.
-Defined.
-
-Lemma prebisim_babyip:
-  forall q1 q2,
-    interp_conf_rel' {| cr_st := {|
-                        cs_st1 := {|
-                          st_state := inl (inl (BabyIP1.Start));
-                          st_buf_len := 0;
-                        |};
-                        cs_st2 := {|
-                          st_state := inl (inr (BabyIP2.Start));
-                          st_buf_len := 0;
-                        |};
-                      |};
-                      cr_ctx := BCEmp;
-                      cr_rel := btrue;
-                   |} q1 q2 ->
-  pre_bisimulation A
-                   (projT1 r_states)
-                   (wp (a :=A))
-                   []
-                   (mk_init _ _ _ _ BabyIP.aut BabyIP1.Start BabyIP2.Start)
-                   q1 q2.
+Lemma babyip_equiv:
+  lang_equiv_state
+    (P4A.interp BabyIP1.aut)
+    (P4A.interp BabyIP2.aut)
+    BabyIP1.Start
+    BabyIP2.Start.
 Proof.
-  idtac "running babyip bisimulation".
-
-  intros.
-  set (rel0 := (mk_init _ _ _ _ BabyIP.aut BabyIP1.Start BabyIP2.Start)).
-  vm_compute in rel0.
-  subst rel0.
-
-  time "build phase" repeat (time "single step" run_bisim).
-  time "close phase" close_bisim.
-Time Admitted.
+  solve_lang_equiv_state.
+Time Qed.
+Print Assumptions babyip_equiv.
--- lib/Benchmarks/MPLSVectorizedProof.v
+++ lib/Benchmarks/MPLSVectorizedProof.v
@@ -1,65 +1,13 @@
 Require Import Leapfrog.Benchmarks.ProofHeader.
 Require Import Leapfrog.Benchmarks.MPLSVectorized.
 
-Require Import Coq.Arith.PeanoNat.
-
-Require Import Leapfrog.Utils.FunctionalFP.
-
-
-Notation A := MPLSVect.aut.
-Notation conf := (P4automaton.configuration (P4A.interp A)).
-Notation start_left :=  Plain.ParseMPLS.
-Notation start_right := Unrolled.ParseMPLS.
-
-Definition r_states : {r : Reachability.state_pairs A & Reachability.reachable_states_wit start_left start_right r}.
-  econstructor.
-  unfold Reachability.reachable_states_wit.
-  solve_fp_wit.
-Defined.
-
-Lemma init_states_wf:
-  Reachability.valid_state_pair (Reachability.build_state_pair A start_left start_right).
+Lemma mpls_equiv:
+  lang_equiv_state
+    (P4A.interp Plain.aut)
+    (P4A.interp Unrolled.aut)
+    Plain.ParseMPLS
+    Unrolled.ParseMPLS.
 Proof.
-  vm_compute; Lia.lia.
-Qed.
-
-Declare ML Module "mirrorsolve".
-
-Lemma prebisim_mpls_unroll:
-  forall q1 q2,
-    interp_conf_rel' {| cr_st := {|
-                        cs_st1 := {|
-                          st_state := inl (inl (start_left));
-                          st_buf_len := 0;
-                        |};
-                        cs_st2 := {|
-                          st_state := inl (inr (start_right));
-                          st_buf_len := 0;
-                        |};
-                      |};
-                      cr_ctx := BCEmp;
-                      cr_rel := btrue;
-                    |} q1 q2 ->
-  pre_bisimulation A
-                   (projT1 r_states)
-                   (wp (a := A))
-                   []
-                   (mk_init _ _ _ _ A start_left start_right)
-                   q1 q2.
-Proof.
-  intros.
-
-  pose proof (Reachability.reachable_states_wit_conv init_states_wf (projT2 r_states)) as Hr.
-
-  unfold mk_init.
-  rewrite Hr.
-  clear Hr.
-
-  set (foo := (List.nodup (conf_rel_eq_dec (a:=A)) (mk_partition _ _ _ _ _ _))).
-  vm_compute in foo.
-  subst foo.
-
-  time "build phase" repeat (time "single step" run_bisim).
-  time "close phase" close_bisim.
-Time Admitted.
+  solve_lang_equiv_state.
+Time Qed.
 
